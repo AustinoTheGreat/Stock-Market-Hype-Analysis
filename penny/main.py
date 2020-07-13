81e495720845eef6ccdf9d5f10cde7205a64f4d7
AustinoTheGreat <53872659+AustinoTheGreat@users.noreply.github.com>
2020-07-13 02:41:05 -0400

    Update main.py

--- penny/main.py
+++ penny/main.py
@@ -23,6 +23,9 @@ with open(filename) as f:
 content = [x.split("|", 1)[0] for x in content]
 content.pop(0)
 content.remove("HUGE")
+content.remove("AMCIW")
+content.remove("ROSEW")
+content.remove("KBLMW")
 
 class stock:
   def __init__(self, ticker, r, averageDailyVolume10Day, volume, fiftyDayAverage, ask, open, dayLow, dayHigh):
@@ -41,15 +44,15 @@ existingPennys = ""
 
 
 
-Rurl = ["https://api.pushshift.io/reddit/search/submission/?subreddit=pennystocks&after=1d&before=0d&limit=1000",
-        "https://api.pushshift.io/reddit/search/submission/?subreddit=pennystocks&after=2d&before=1d&limit=1000",
-        "https://api.pushshift.io/reddit/search/submission/?subreddit=pennystocks&after=3d&before=2d&limit=1000",
-        "https://api.pushshift.io/reddit/search/submission/?subreddit=pennystocks&after=4d&before=3d&limit=1000",
-        "https://api.pushshift.io/reddit/search/submission/?subreddit=pennystocks&after=5d&before=4d&limit=1000",
-        "https://api.pushshift.io/reddit/search/submission/?subreddit=pennystocks&after=6d&before=5d&limit=1000",
-        "https://api.pushshift.io/reddit/search/submission/?subreddit=pennystocks&after=7d&before=6d&limit=1000"]
+# Rurl = ["https://api.pushshift.io/reddit/search/submission/?subreddit=pennystocks&after=1d&before=0d&limit=1000",
+#         "https://api.pushshift.io/reddit/search/submission/?subreddit=pennystocks&after=2d&before=1d&limit=1000",
+#         "https://api.pushshift.io/reddit/search/submission/?subreddit=pennystocks&after=3d&before=2d&limit=1000",
+#         "https://api.pushshift.io/reddit/search/submission/?subreddit=pennystocks&after=4d&before=3d&limit=1000",
+#         "https://api.pushshift.io/reddit/search/submission/?subreddit=pennystocks&after=5d&before=4d&limit=1000",
+#         "https://api.pushshift.io/reddit/search/submission/?subreddit=pennystocks&after=6d&before=5d&limit=1000",
+#         "https://api.pushshift.io/reddit/search/submission/?subreddit=pennystocks&after=7d&before=6d&limit=1000"]
 
-# Rurl = ["https://api.pushshift.io/reddit/search/submission/?subreddit=pennystocks&after=1h&before=0d&limit=1000"]
+Rurl = ["https://api.pushshift.io/reddit/search/submission/?subreddit=pennystocks&after=1d&before=0d&limit=1000"]
 
 for u in Rurl:
     r = requests.get(u)
@@ -61,23 +64,30 @@ for u in Rurl:
             if re.search(rf"(\b(?=\w)|$){x}\b(?!\w)", data["data"][i]["title"], re.IGNORECASE):
             # if r"[$]|^|\s" + x.lower() + r"$|\s" in data["data"][i]["title"].lower():
                 if x in existingPennys:
-                    for x in pennys:
-                        if x == x.ticker:
-                            x.r = x.r + 1
+                    for y in pennys:
+                        if x == y.ticker:
+                            y.r = y.r + 1
                             break
                 else:
                     existingPennys = existingPennys + " " + x
                     cur = yf.Ticker(x)
                     print(x)
                     print(i)
-                    if cur.info["dayLow"] < 5:
+                    if cur.info["dayLow"] < 7:
                         pennys.append(stock(x, 1, cur.info["averageDailyVolume10Day"], cur.info["volume"], 
                             cur.info["fiftyDayAverage"], cur.info["ask"], cur.info["open"], cur.info["dayLow"], 
                             cur.info["dayHigh"]))
                     print("a")
 
 for x in pennys:
-    print(x.ticker + ": " + x.r)
+    print(x.ticker + ": " + str(x.r))
+
+with open('data.csv', mode='w') as dataFile:
+    dataFileWriter = csv.writer(dataFile)
+    dataFileWriter.writerow(["Ticker", "Pennystocks Mentions", "averageDailyVolume10Day", "volume", "fiftyDayAverage", "ask", "open", "dayLow", "dayHigh"])
+
+    for x in pennys:
+        dataFileWriter.writerow([x.ticker, x.r, x.averageDailyVolume10Day, x.volume, x.fiftyDayAverage, x.ask, x.open, x.dayLow, x.dayHigh])
 
 
 
